199cc952f7fc3c4d3006420a4bdb89a8f493957d
fede977 <rullofederico16@gmail.com>
2019-03-10 19:20:59 +0000

    created Panel

--- GameCode/Panel.java
+++ GameCode/Panel.java
@@ -1,5 +1,30 @@
 package GameCode;
 
+import java.awt.*;
+import javax.swing.*;
+
+
 public class Panel{
     
+
+    private static void createFrame(){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel();
+        label.setSize(new Dimension(180,140));
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        javax.swing.SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                createFrame();
+            }
+
+        });
+    }
 }
